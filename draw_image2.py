4620f8acc3940cf531823233f5917351e91f79da
Evrash <Evrashreg@gmail.com>
2025-03-01 02:50:49 +0300

    Fix OOP based draw image file

--- draw_image2.py
+++ draw_image2.py
@@ -1,11 +1,11 @@
 import random
 import os
 import datetime
+from pathlib import Path
 
 from PIL import Image
 from image_config import ImageConfig
 from config import settings
-# from test import light_template
 
 
 class LightImage(object):
@@ -20,52 +20,49 @@ class LightImage(object):
         self.light_template = light_template
         self.org = org
 
+    # def paste_element_on_image(self, image:Image, element_path: Path, position:tuple[int, int]):
+    #     open_image = Image.open(element_path).convert('RGBA')
+    #     image.paste(open_image, position, open_image)
+
     def draw_image(self):
         """
-        Создаёт итоговое изображение. Берётся основа, на неё наносятся другие изображения.
+        Создаёт итоговое изображение. Берётся основа, на неё наносятся другие детали изображения.
         Передаётся строка содержащая цвет светофора для каждой группы крови.
         Изображения делятся на зависящие и не зависящие от группы крови.
         :return:
         """
         if not settings.base_dir / f'img/{self.org}':
             os.makedirs(settings.base_dir / f'img/{self.org}')
+        template_path: Path =settings.base_dir / 'img_templates' / self.image_template.dir
         image: Image = Image.new('RGBA', self.image_template.size)
-        image = Image.alpha_composite(image, Image.open(
-            f'img_templates/{self.image_template.dir}/back/{random.choice(self.image_template.elements.back)}').convert('RGBA'))
+        image = Image.alpha_composite(image, Image.open(template_path / 'back' /
+                                                        random.choice(self.image_template.elements.back)).convert('RGBA'))
 
         # Размещаем элементы, зависимые от шаблона
-        # print(self.image_template.elements.main.items())
         for element_name, element_value in self.image_template.elements.main.items():
-            print(element_name)
-            print(element_value)
-            print(type(element_value))
             for group, params in self.light_template.items():
-                print(group)
-                path = f'img_templates/{self.image_template.dir}/{element_name}/{getattr(element_value,group).image[params]}'
-                open_image = Image.open(path).convert("RGBA")
+                path =  template_path / f'{element_name}' / getattr(element_value,group).image[params]
+                open_image = Image.open(path).convert('RGBA')
                 position = (getattr(element_value,group).position.x, getattr(element_value,group).position.y)
                 image.paste(open_image, position, open_image)
-            # for name in element_value:
-            #     path = f'img_templates/{self.image_template.dir}/{element_name}/{name.self.light_template[str(name)]}'
-            #     open_image = Image.open(path).convert("RGBA")
-                # image.paste(open_image, (value.position.x, value.position.y), open_image)
-            # for key, value in element_value.items():
-            #     path = f'img_templates/{self.image_template.dir}/{element_name}/{value.image[self.light_template[key]]}'
-            #     open_image = Image.open(path).convert("RGBA")
-            #     image.paste(open_image, (value.position.x, value.position.y), open_image)
 
-        # Размещаем элементы не зависимые от шаблона
-        # for element_name, element_value in self.image_template.elements.additional.items():
-        #     if element_value.random == 0: # Нужно ли размещать элементы в случайном порядке
-        #         for key, value in element_value.items.items() :
-        #             path = f'img_templates/{self.image_template.dir}/{element_name}/{value.image}'
-        #             image.paste(Image.open(path), (value.position.x, value.position.y), Image.open(path))
-        #     else:
-        #         folder_path = f'img_templates/{self.image_template.dir}/{element_name}/'
-        #         images = os.listdir(folder_path)
-        #         for position in element_value.position:
-        #             image_path = folder_path + random.choice(images)
-        #             image.paste(Image.open(image_path), position, Image.open(image_path))
+        # Размещаем элементы не зависимые от групп крови
+        if self.image_template.elements.additional.positional:
+            for elements_name, elements_value in self.image_template.elements.additional.positional.items():
+                for element_name, element_value in elements_value.items.items():
+                    path = template_path / f'{elements_name}' / f'{element_value.image}'
+                    open_image = Image.open(path).convert('RGBA')
+                    image.paste(open_image, (element_value.position.x, element_value.position.y), open_image)
 
-        image_name = f'img/org1/{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}-{self.image_template.name}.png'
+        if self.image_template.elements.additional.random:
+            for elements_name, elements_value in self.image_template.elements.additional.random.items():
+                folder_path = template_path / f'{elements_name}/'
+                images = os.listdir(folder_path)
+                for position in elements_value.position:
+                    path = folder_path / random.choice(images)
+                    open_image = Image.open(path).convert('RGBA')
+                    image.paste(open_image, position, open_image)
+
+        image_name = (settings.base_dir / f'img/{self.org}'/
+                      f'{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}-{self.image_template.name}.png')
         image.save(image_name)
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from draw_image2 import LightImage
 
 light_template = {'o_plus': 'red', 'o_minus':'green',
-                  'a_plus': 'red', 'a_minus':'green',
-                  'b_plus': 'red', 'b_minus':'green',
-                  'ab_plus': 'red', 'ab_minus':'green'}
+                  'a_plus': 'yellow', 'a_minus':'red',
+                  'b_plus': 'green', 'b_minus':'yellow',
+                  'ab_plus': 'green', 'ab_minus':'red'}
 
 image = LightImage('color_drops', light_template, 'org1')
 image.draw_image()
