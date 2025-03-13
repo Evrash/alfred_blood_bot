0949f2a5e32634780ff3cd15318b5f8cb45be93e
Evrash <Evrashreg@gmail.com>
2025-03-13 03:21:49 +0300

    Add main config file

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 from pathlib import Path
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).parent
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file= (BASE_DIR / '.env.template', BASE_DIR / '.env'),
+        case_sensitive=False,
+        env_nested_delimiter='__',
+        env_prefix='BOT_CONFIG__'
+    )
     base_dir:Path = BASE_DIR
+    token: str = ''
 
 settings = Settings()
