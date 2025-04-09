298e52777632360a48cc4d82882d6ae85030dd71
Anton Abramov <evrashreg@gmail.com>
2025-04-09 21:19:14 +0300

    renew all tables

--- alembic/versions/2025_04_09_1106-57941eb25b8b_create_tables.py
+++ alembic/versions/2025_04_09_1106-57941eb25b8b_create_tables.py
@@ -1,8 +1,8 @@
-"""create organization table
+"""Create tables
 
-Revision ID: 2a7815122ddb
+Revision ID: 57941eb25b8b
 Revises:
-Create Date: 2025-03-17 00:28:20.594280
+Create Date: 2025-04-09 11:06:17.513862
 
 """
 
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = "2a7815122ddb"
+revision: str = "57941eb25b8b"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -23,13 +23,12 @@ def upgrade() -> None:
     """Upgrade schema."""
     op.create_table(
         "organisations",
-        sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("name", sa.String(), nullable=False),
         sa.Column("vk_token", sa.String(), nullable=True),
         sa.Column("vk_group_id", sa.Integer(), nullable=True),
         sa.Column("vk_last_light_post", sa.String(), nullable=True),
         sa.Column("vk_is_pin_image", sa.Boolean(), nullable=False),
-        sa.Column("vk_template", sa.Integer(), nullable=True),
+        sa.Column("vk_template", sa.String(), nullable=True),
         sa.Column("yd_login", sa.String(), nullable=True),
         sa.Column("yd_pass", sa.String(), nullable=True),
         sa.Column("yd_station_id", sa.String(), nullable=True),
@@ -37,9 +36,12 @@ def upgrade() -> None:
         sa.Column("hashtag", sa.String(), nullable=True),
         sa.Column("last_create_date", sa.DateTime(), nullable=True),
         sa.Column("last_group_state", sa.String(), nullable=True),
+        sa.Column("last_image_name", sa.String(), nullable=True),
+        sa.Column("last_yd_str", sa.String(), nullable=True),
         sa.Column("start_text", sa.String(), nullable=True),
         sa.Column("end_text", sa.String(), nullable=True),
         sa.Column("notification_time", sa.DateTime(), nullable=True),
+        sa.Column("id", sa.Integer(), nullable=False),
         sa.Column(
             "created_at",
             sa.DateTime(),
@@ -54,7 +56,38 @@ def upgrade() -> None:
         ),
         sa.PrimaryKeyConstraint("id", name=op.f("pk_organisations")),
     )
+    op.create_table(
+        "users",
+        sa.Column("tg_id", sa.String(), nullable=False),
+        sa.Column("organisation_id", sa.Integer(), nullable=True),
+        sa.Column("is_admin", sa.Boolean(), nullable=False),
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column(
+            "created_at",
+            sa.DateTime(),
+            server_default=sa.text("(CURRENT_TIMESTAMP)"),
+            nullable=False,
+        ),
+        sa.Column(
+            "updated_at",
+            sa.DateTime(),
+            server_default=sa.text("(CURRENT_TIMESTAMP)"),
+            nullable=False,
+        ),
+        sa.ForeignKeyConstraint(
+            ["organisation_id"],
+            ["organisations.id"],
+            name=op.f("fk_users_organisation_id_organisations"),
+        ),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_users")),
+        sa.UniqueConstraint(
+            "organisation_id", name=op.f("uq_users_organisation_id")
+        ),
+        sa.UniqueConstraint("tg_id", name=op.f("uq_users_tg_id")),
+    )
+
 
 def downgrade() -> None:
     """Downgrade schema."""
+    op.drop_table("users")
     op.drop_table("organisations")
